cancel bbox normalization. fix render to xywh

render_dataset_examples.py
@@ -33,20 +33,27 @@ def draw_text_on_bounding_box(image, ymin, xmin, color, display_str_list=(), fon
 
     text_margin_factor = 0.05
 
-    text_widths, text_heights = zip(*[font.getsize(display_str) for display_str in display_str_list])
+    left, top, right, bottom = zip(*[font.getbbox(display_str) for display_str in display_str_list])
+    text_heights= tuple(map(lambda i, j: i - j, bottom, top))
+    text_widths = tuple(map(lambda i, j: i - j, right, left))
+
     text_margins = np.ceil(text_margin_factor * np.array(text_heights))
     text_bottoms = ymin * (ymin > text_heights) + (ymin + text_heights) * (ymin <= text_heights)
 
     for idx, (display_str, xmint, text_bottom, text_width, text_height, text_margin) in enumerate(
             zip(display_str_list, xmin, text_bottoms, text_widths, text_heights, text_margins)):
-        text_width, text_height = font.getsize(display_str)
+
+        left, top, right, bottom = font.getbbox(display_str)
+        text_height = bottom - top
+        text_width = right - left
+
         text_margin = np.ceil(text_margin_factor * text_height)
 
         draw.rectangle(((xmint, text_bottom - text_height - 2 * text_margin),
                         (xmint + text_width + text_margin, text_bottom)),
                        fill=color)
 
-        draw.text((xmint + text_margin, text_bottom - text_height - text_margin),
+        draw.text((xmint + text_margin, text_bottom - text_height - 3 * text_margin),
                   display_str,
                   fill="black",
                   font=font)
@@ -56,8 +63,8 @@ def draw_text_on_bounding_box(image, ymin, xmin, color, display_str_list=(), fon
 def draw_bounding_box(image, boxes, color, thickness=1):
     draw = ImageDraw.Draw(image)
     for box in boxes:
-        xmin, ymin, xmax, ymax = box
-        draw.line([(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax, ymin),
+        xmin, ymin, w, h = box
+        draw.line([(xmin, ymin), (xmin, ymin+h), (xmin+w, ymin+h), (xmin+w, ymin),
                    (xmin, ymin)],
                   width=thickness,
                   fill=color)
@@ -88,25 +95,24 @@ def main():
 
     for idx in range(num_of_images):
         image_index = np.random.randint(start_index, len(annotations)) if random_select else start_index + idx
-        line = annotations[image_index]
-        boxes = line['bboxes']
-
-        image_path = f'{images_dir}{line["image_filename"]}'
-
+        image_record = annotations['images'][image_index]
+        annotation_records = [annotation for  annotation in annotations['annotations'] if annotation['image_id'] == image_record['id']]
+        image_path = f'{images_dir}{image_record["file_name"]}'
         image = Image.open(image_path)
         colors = list(ImageColor.colormap.values())
         color = colors[0]
+        bboxes = [annotation_record['bbox'] for annotation_record in annotation_records]
+        bboxes = np.array(bboxes)
+        bboxes[..., 0:3:2] = bboxes[..., 0:3:2]
+        bboxes[..., 1:4:2] = bboxes[..., 1:4:2]
+        annotated_bbox_image = draw_bounding_box(image, bboxes, color, thickness=1)
+        category_ids = [annotation_record['category_id'] for annotation_record in annotation_records]
 
-        boxes = np.array(boxes)
-        width, height = image.size
-        boxes[..., 0:3:2] = boxes[..., 0:3:2] * width
-        boxes[..., 1:4:2] = boxes[..., 1:4:2] * height
-        annotated_bbox_image = draw_bounding_box(image, boxes, color, thickness=1)
+        category_names = [category['name'] for category in annotations['categories'] if category['id'] in category_ids]
 
-        classes = [label for label in line['labels']]
 
-        annotated_text_image = draw_text_on_bounding_box(annotated_bbox_image, boxes[..., 1], boxes[..., 0], color,
-                                                         classes, font_size=15)
+        annotated_text_image = draw_text_on_bounding_box(annotated_bbox_image, bboxes[..., 1], bboxes[..., 0], color,
+                                                         category_names, font_size=15)
 
         figure(figsize=(10, 10))
         plt.imshow(annotated_text_image)

src/create_shapes_dataset.py
@@ -37,6 +37,25 @@ def compute_iou(box1, box2):
 
 def create_bbox(image_size, bboxes, shape_width_choices, axis_ratio, iou_thresh, margin_from_edge,
                 size_fluctuation=0.01):
+    """
+                
+    :param image_size: Canvas size
+    :type image_size: 
+    :param bboxes: 
+    :type bboxes: 
+    :param shape_width_choices: 
+    :type shape_width_choices: 
+    :param axis_ratio: 
+    :type axis_ratio: 
+    :param iou_thresh: 
+    :type iou_thresh: 
+    :param margin_from_edge: 
+    :type margin_from_edge: 
+    :param size_fluctuation: 
+    :type size_fluctuation: 
+    :return: 
+    :rtype: 
+    """
     max_count = 10000
     count = 0
     # Iterative loop to find location for shape placement i.e. center. Max iou with prev boxes should be g.t. iou_thresh
@@ -48,7 +67,7 @@ def create_bbox(image_size, bboxes, shape_width_choices, axis_ratio, iou_thresh,
         radius = np.array([shape_width / 2, shape_height / 2])
         center = np.random.randint(
             low=radius + margin_from_edge, high=np.floor(image_size - radius - margin_from_edge), size=2)
-        bbox_sides = radius
+        # bbox_sides = radius
         new_bbox = np.concatenate(np.tile(center, 2).reshape(2, 2) +
                                   np.array([np.negative(radius), radius]))
         # iou new shape bbox with all prev bboxes. skip shape if max iou > thresh - try another placement for shpe
@@ -143,7 +162,7 @@ def make_image(shapes, image_size, min_objects_in_image, max_objects_in_image, b
               bboxes[:, 2] - bboxes[:, 0] + 2 * bbox_margin,
               bboxes[:, 3] - bboxes[:, 1] + 2 * bbox_margin]  # / np.tile(image_size,2)
 
-    bboxes = np.stack(bboxes, axis=1) / np.tile(image_size, 2)
+    bboxes = np.stack(bboxes, axis=1)
 
     return image, bboxes, objects_categories_names
 
@@ -170,6 +189,16 @@ def fill_categories_records(shapes):
 
 
 def create_dataset(config_file, shapes_file):
+    """
+    :param config_file: Configuration file. Holds common configs, e.g. image size, num of object in image
+    :type config_file: yaml
+    :param shapes_file: Configuration file. Defines characteristics of shapes objects included - se.g. type, ize, color
+    :type shapes_file: yaml
+    :return:  Creates 3 output files: 1. An annotation json file, in the format of coco dataset. Data is arranged a a dict object with 5 elements:
+        info, licenses, images, categories, annotations 2. class_names_file, which holds the class names table.
+        This table is used by inference for category id decoding. 3. Images files.
+    :rtype:
+    """
     with open(shapes_file, 'r') as stream:
         shapes = yaml.safe_load(stream)
 