cancel bbox normalization. fix render to xywh

render_dataset_examples.py
@@ -103,13 +103,20 @@ def main():
         color = colors[0]
         bboxes = [annotation_record['bbox'] for annotation_record in annotation_records]
         bboxes = np.array(bboxes)
-        bboxes[..., 0:3:2] = bboxes[..., 0:3:2]
-        bboxes[..., 1:4:2] = bboxes[..., 1:4:2]
+        # width, height = image.size
+        bboxes[..., 0:3:2] = bboxes[..., 0:3:2] # * width
+        bboxes[..., 1:4:2] = bboxes[..., 1:4:2] #* height
         annotated_bbox_image = draw_bounding_box(image, bboxes, color, thickness=1)
         category_ids = [annotation_record['category_id'] for annotation_record in annotation_records]
-
+        # ss = annotations['categories']
+        # for category in annotations['categories']:
+        #     if category['id'] in category_ids:
+        #         print()
         category_names = [category['name'] for category in annotations['categories'] if category['id'] in category_ids]
 
+        # figure(figsize=(10, 10))
+        # plt.imshow(annotated_bbox_image)
+        # plt.show()
 
         annotated_text_image = draw_text_on_bounding_box(annotated_bbox_image, bboxes[..., 1], bboxes[..., 0], color,
                                                          category_names, font_size=15)

src/create_shapes_dataset.py
@@ -162,7 +162,7 @@ def make_image(shapes, image_size, min_objects_in_image, max_objects_in_image, b
               bboxes[:, 2] - bboxes[:, 0] + 2 * bbox_margin,
               bboxes[:, 3] - bboxes[:, 1] + 2 * bbox_margin]  # / np.tile(image_size,2)
 
-    bboxes = np.stack(bboxes, axis=1)
+    bboxes = np.stack(bboxes, axis=1) #/ np.tile(image_size, 2)
 
     return image, bboxes, objects_categories_names
 