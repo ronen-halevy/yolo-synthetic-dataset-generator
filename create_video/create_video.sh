caff46f13ec5591870839892a73cdf64cfce614f
ronen-halevy <ronen567@gmail.com>
2023-05-18 17:08:13 +0300

    create video

--- create_video/create_video.sh
+++ create_video/create_video.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # creates a video from a sequence of images:
-
-ffmpeg -framerate 2 -i %06d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
+#ffmpeg -framerate 2 -i dataset/train/img_%06d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
+ffmpeg -framerate 2 -i dataset/red_circle/img_%06d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p red_circle.mp4
+ffmpeg -framerate 2 -i dataset/red_circle/img_00006%01d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p red_circle_short.mp4
