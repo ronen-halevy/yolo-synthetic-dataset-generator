9bda918ef3fb1efef0739d43a5c7079d10c5865b
ronen-halevy <ronen567@gmail.com>
2023-07-29 23:55:57 +0300

    minor arrangement

--- render.py
+++ render.py
@@ -32,22 +32,22 @@ def main():
     category_names_file = config['category_names_file']
     with open(category_names_file) as f:
         category_names_table = f.read().splitlines()
+    out_formats_table = config['label_file_formats']
+    if 'coco_detection_datase' in out_formats_table.keys():
+        draw_coco_detection_dataset_example(out_formats_table['coco_detection_datase']['annotations_path'], category_names_table, output_dir)
 
-    if 'coco_detection_datase' in config['label_file_formats'].keys():
-        draw_coco_detection_dataset_example(config['label_file_formats']['coco_detection_datase']['annotations_path'], category_names_table, output_dir)
-
-    if 'yolov5_detection_format' in config['label_file_formats'].keys():
-        image_dir = config['label_file_formats']['yolov5_detection_format']['image_dir']
-        label_dir = config['label_file_formats']['yolov5_detection_format']['label_dir']
+    if 'yolov5_detection_format' in out_formats_table.keys():
+        image_dir = out_formats_table['yolov5_detection_format']['image_dir']
+        label_dir = out_formats_table['yolov5_detection_format']['label_dir']
         draw_detection_dataset_example(image_dir, label_dir, category_names_table, output_dir)
 
-    if 'single_label_file_format' in config['label_file_formats'].keys():
-        label_path=config['label_file_formats']['single_label_file_format']['label_path']
+    if 'single_label_file_format' in out_formats_table.keys():
+        label_path=out_formats_table['single_label_file_format']['label_path']
         draw_detection_single_file_dataset_example(label_path, category_names_table, output_dir)
 
-    if 'yolov5_segmentation_format' in config['label_file_formats'].keys():
-        image_dir = config['label_file_formats']['yolov5_segmentation_format']['image_dir']
-        label_dir = config['label_file_formats']['yolov5_segmentation_format']['label_dir']
+    if 'yolov5_segmentation_format' in out_formats_table.keys():
+        image_dir = out_formats_table['yolov5_segmentation_format']['image_dir']
+        label_dir = out_formats_table['yolov5_segmentation_format']['label_dir']
         draw_segmentation_dataset_example(image_dir, label_dir, category_names_table, output_dir)
 
 
