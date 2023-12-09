c2f303d375a7b956a0d52807a258bd60989a8e63
ronen-halevy <ronen567@gmail.com>
2023-12-09 18:44:47 +0200

    create anchors updated

---
+++ create_anchors/create_yolov_anchors.py
@@ -0,0 +1,164 @@
+#! /usr/bin/env python
+# coding=utf-8
+# ================================================================
+#   Copyright (C) 2022 . All rights reserved.
+#
+#   File name   : create_yolov_anchors.py
+#   Author      : ronen halevy 
+#   Created date:  5/3/22
+#   Description :
+#
+# ================================================================
+# TODO - set in descending order!
+import tensorflow as tf
+import yaml
+
+import numpy as np
+from sklearn.cluster import KMeans
+import argparse
+import pathlib
+from pathlib import Path
+import glob
+
+import os
+import matplotlib.pyplot as plt
+
+
+def plot_scatter_graph(w_h, kmeans):
+    image_width = 2 * max(w_h[..., 0])
+    image_height = 2 * max(w_h[..., 1])
+
+    plt.scatter(w_h[..., 0], w_h[..., 1], c=kmeans.labels_, alpha=0.5)
+    plt.xlabel("width")
+    plt.ylabel("height")
+    plt.title('K-means Clustering of Boxes Widths and Heights')
+    plt.figure(figsize=(10, 10))
+    plt.show()
+    plt.savefig('books_read.png')
+def sort_anchors(anchors):
+    anchors_sorted = anchors[(anchors[:, 0] * anchors[:, 1]).argsort()]
+    return anchors_sorted
+
+
+def arrange_wh_array(labels):
+    w_h_tuples = (labels[..., 2] - labels[..., 0], labels[..., 3] - labels[..., 1])
+    widths = tf.expand_dims(w_h_tuples[0], axis=-1)
+    heights = tf.expand_dims(w_h_tuples[1], axis=-1)
+    w_h = tf.concat([widths, heights], axis=-1)
+    mask = tf.reduce_all(w_h != [0., 0.], axis=-1)
+    w_h = w_h[mask]
+    return w_h
+
+
+def creat_yolo_anchors(w_h, n_clusters):
+    """
+    Create anchors by sklearn  KMeans
+    :param w_h:
+    :type w_h:
+    :param n_clusters:
+    :type n_clusters:
+    :return:
+    :rtype:
+    """
+    kmeans = KMeans(n_clusters=n_clusters)  # Construct with num of clusters (in yolo - 9 (3*3))
+    kmeans.fit(w_h)
+    anchors = kmeans.cluster_centers_  # coordinates of cluster' centers
+    sorted_anchors = sort_anchors(anchors).astype(np.float32)
+    return sorted_anchors
+
+
+def segments2boxes(segments):
+    # Convert segment labels to box labels, i.e. (cls, xy1, xy2, ...) to (cls, xywh)
+    boxes = []
+    for s in segments:
+        x, y = s.T  # segment xy
+        boxes.append([x.min(), y.min(), x.max(), y.max()])  # cls, xyxy
+    return boxes  # cls, xywh
+
+
+def _list_label_files(path, ext_list):
+    """
+    Return a list of all files with ext_list extension located at path
+    :param path: location of input files, string.
+    :param ext_list: dile extension to search for, str
+    :return: list of file names found
+    """
+    try:
+        f = []  # label files
+        for p in path if isinstance(path, list) else [path]:
+            p = Path(p)  # os-agnostic
+            if p.is_dir():  # dir
+                f += glob.glob(str(p / '**' / '*.*'), recursive=True)
+                # f = list(p.rglob('*.*'))  # pathlib
+            elif p.is_file():  # file
+                with open(p) as t:
+                    t = t.read().strip().splitlines()
+                    parent = str(p.parent) + os.sep
+                    f += [x.replace('./', parent, 1) if x.startswith('./') else x for x in t]  # to global path
+            else:
+                raise FileNotFoundError(f'{p} does not exist')
+        lb_files = sorted(x.replace('/', os.sep) for x in f if x.split('.')[-1].lower() in ext_list)
+        assert lb_files, f'{"prefix"}No label files found'
+        return lb_files
+    except Exception as e:
+        raise Exception(f'Error loading data from {path}: {e}') from e
+
+
+def read_label_from_file(fname):
+    """
+    Reads segments label file, retrun class and bbox.
+    Input File format-a row per object structured: class, sx1,sy1....sxn,syn
+    :param fname: labels file name, str. Fi
+    :return:
+    lb:  tensor of class,bbox. shape: [nt,5], tf.float32, where nt - num of objects rows in file
+    """
+    with open(fname) as f:
+        lb = [x.split() for x in f.read().strip().splitlines() if len(x)]
+        if any(len(x) > 6 for x in lb):  # is segment
+            classes = np.array([x[0] for x in lb], dtype=np.float32)
+            # img_index = tf.tile(tf.expand_dims(tf.constant([idx]), axis=-1), [classes.shape[0], 1])
+            segments = [np.array(x[1:], dtype=np.float32).reshape(-1, 2) for x in lb]  # (cls, xy1...)
+            lb = np.concatenate((classes.reshape(-1, 1), segments2boxes(segments)),
+                                1)  # (cls, xywh)
+        lb = np.array(lb, dtype=np.float32)
+    return lb
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default='create_anchors_config.yaml',
+                        help='yaml config file')
+
+    args = parser.parse_args()
+    config_file = args.config
+    with open(config_file, 'r') as stream:
+        config_file = yaml.safe_load(stream)
+
+    n_clusters = config_file['n_clusters']
+    anchors_out_file = config_file['anchors_out_file']
+
+    ext_list = 'txt'
+    path = config_file['labels_dir']
+    label_files = _list_label_files(path, ext_list)
+
+    labels=np.zeros([0,5])
+    for idx, label_file in enumerate(label_files):
+        # extract class, bbox and segment from label file:
+        label = read_label_from_file(label_file)
+        labels=np.concatenate([labels, label], axis=0) # labels shape: [b*nt,5] where b nof label files
+
+    wh = arrange_wh_array(labels)
+
+
+    anchors = creat_yolo_anchors(wh, n_clusters)
+    base_dir, fname = os.path.split(anchors_out_file)
+    pathlib.Path(base_dir).mkdir(parents=True, exist_ok=True)
+
+    np.savetxt(anchors_out_file, anchors, delimiter=',', fmt='%10.5f')
+    print(f'result anchors:\n{anchors}')
+    print(f'anchors_out_file: {anchors_out_file}')
+
+
+if __name__ == '__main__':
+    main()
+
