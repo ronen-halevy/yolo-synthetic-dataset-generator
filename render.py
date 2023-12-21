c712b00ce9c955fef82e4db4981612cddcb8d5a2
ronen-halevy <ronen567@gmail.com>
2023-12-21 18:03:28 +0200

    render separated from creation

--- render.py
+++ render.py
@@ -32,7 +32,6 @@ def render(nexamples, labels_file_format, image_dir, labels_dir, output_dir, cat
             draw_coco_detection_dataset_example(annotations_path, category_names_table, f'{output_dir}/coco')
         if labels_file_format == 'detection_yolov5':
             draw_detection_dataset_example(image_dir, labels_dir, category_names_table, f'{output_dir}/det1')
-
         if labels_file_format == 'detection_unified_textfile':
             draw_detection_single_file_dataset_example(labels_dir, image_dir, category_names_table,
                                                        f'{output_dir}/det2')
@@ -44,24 +43,34 @@ if __name__ == "__main__":
     config_file = './config/dataset_config.yaml'
     with open(config_file, 'r') as stream:
         config = yaml.safe_load(stream)
-    split = 'train'  # can be 'train', 'test', 'valid'
-    image_dir = config['image_dir']  # './dataset/images/{split}/'
-    images_out_dir = f'{image_dir.replace("{split}", split)}'
-    labels_dir = config['labels_dir'].replace("{split}", split)
-
-    coco_json_labels_file_pth = config['coco_json_labels_file_pth']
-    labels_out_dir = coco_json_labels_file_pth.replace('{split}', split)
-
-    labels_out_dir = config['coco_json_labels_file_pth']
-    labels_out_dir = labels_out_dir.replace('{split}', split)
-
-    labels_dir = config['labels_dir']
-
-    labels_out_dir = config['labels_all_entries_file']
-    labels_out_dir = labels_out_dir.replace("{split}", split)
-    nexamples = config['nrender_examples'] + 1
-
+    split = config['split_to_render'] # 'train'  # can be 'train', 'test', 'valid'
     labels_file_format = config.get('labels_file_format')
+
+    if labels_file_format in ['segmentation_yolov5', 'detection_yolov5']:
+        labels_dir = config['labels_dir'].replace("{split}", split)
+        images_dir = config["image_dir"].replace("{split}", split)
+
+    elif labels_file_format == 'detection_coco_json_format':
+        coco_json_labels_file_path = config['coco_json_labels_file_path']
+        labels_dir = coco_json_labels_file_path.replace('{split}', split)
+        images_dir=None # complete path within json file
+    elif labels_file_format == 'detection_unified_textfile':
+        labels_dir = config['labels_all_entries_file'].replace("{split}", split)
+        images_dir = config["image_dir"].replace("{split}", split) # an offset for image filenames located in labels
+    else:
+        raise('Unknown or missing labels_file_format! configuration')
+    nexamples = config['nrender_examples'] + 1
+    labels_file_format = config.get('labels_file_format')
+
+    class_names_file=config['category_names_file']
+
+    output_dir = config['test_output_dir']
+    print('\nrendering dataset images with bbox and mask overlays\n')
+    output_dir=increment_path(output_dir)
+
+
+    category_names = [c.strip() for c in open(class_names_file).readlines()]
+    render(nexamples, labels_file_format, images_dir, labels_dir, f'{output_dir}/{split}', category_names, split)
 
     # render =
     # render(image_dir, )
