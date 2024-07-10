b485cd615164b0fcf18aa25431d0019e7fbfcc29
ronen-halevy <ronen567@gmail.com>
2024-07-10 18:24:25 +0300

    fix-remove bracketsfrom kpts entries

--- src/kpts_detection_labels_utils.py
+++ src/kpts_detection_labels_utils.py
@@ -1,28 +1,26 @@
 import numpy as np
-def create_detection_kpts_entries(images_bboxes, images_polygons, images_sizes, images_class_ids):
+def create_detection_kpts_entries(batch_bboxes, batch_polygons, batch_sizes, batch_class_ids):
 
     """
     Description: one *.txt file per image,  one row per object, row format: class polygon vertices (x0, y0.....xn,yn)
     normalized coordinates [0 to 1].
     zero-indexed class numbers - start from 0
 
-
     :param images_paths: list of dataset image filenames
-    :param images_polygons: list of per image polygons arrays
-    :param images_class_ids:  list of per image class_ids
+    :param batch_polygons: list of per image polygons arrays
+    :param batch_class_ids:  list of per image class_ids
     :param output_dir: output dir of labels text files
     :return:
     """
-
-    # detection_entries = create_detection_entries(images_bboxes, images_sizes, images_class_ids)
+    # detection_entries = create_detection_entries(batch_bboxes, batch_sizes, batch_class_ids)
     entries=[]
-    for image_polygons, images_size, class_ids, image_detection_entries in zip(images_polygons, images_sizes,
-                                                              images_class_ids, images_bboxes):
-        image_detection_entries=np.array(image_detection_entries)
+    for image_polygons, image_size, class_ids, image_bboxes in zip(batch_polygons, batch_sizes,
+                                                              batch_class_ids, batch_bboxes):
+        image_bboxes=np.array(image_bboxes)
         image_polygons=np.array(image_polygons)
 
-        im_height = images_size[0]
-        im_width = images_size[1]
+        im_height = image_size[0]
+        im_width = image_size[1]
 
         img_kpts = (image_polygons/np.array([im_width, im_height]))
         # concat valid field:
@@ -30,8 +28,10 @@ def create_detection_kpts_entries(images_bboxes, images_polygons, images_sizes, 
         img_kpts = np.concatenate([img_kpts, img_kpts_valid], axis=-1).reshape(img_kpts.shape[0], -1) # flatten kpts per object
 
         img_entries=[]
-        for detection_entry, kpts   in zip(image_detection_entries, img_kpts):
-            entry = f"{detection_entry} {' '.join(str(kpt) for kpt in list(kpts.reshape(-1)))}"
+
+        for bbox, kpts   in zip(image_bboxes, img_kpts):
+            bbox = ' '.join(str( round(vertex, 2)) for vertex in list(bbox))
+            entry = f"{bbox} {' '.join(str( round(kpt, 2)) for kpt in list(kpts.reshape(-1)))}"
             img_entries.append(entry)
         entries.append(img_entries)
     return entries
