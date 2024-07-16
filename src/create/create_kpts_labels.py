3b62cd632188730c50127cf1c87ac9cd18b286a0
ronen-halevy <ronen567@gmail.com>
2024-07-17 00:41:10 +0300

    support kpts 0 padding

--- src/create/create_kpts_labels.py
+++ src/create/create_kpts_labels.py
@@ -13,7 +13,8 @@ class CreatesKptsEntries(CreatePolygons, CreateBboxes):
         batch_labels=self.create_detection_kpts_entries(batch_bboxes, batch_polygons, batch_image_size, batch_categories_ids)
         return batch_polygons, batch_labels, batch_objects_colors, batch_image_size
 
-    def create_detection_kpts_entries(self, batch_bboxes, batch_polygons, batch_img_sizes, batch_class_ids):
+
+    def create_detection_kpts_entries(self, batch_bboxes, batch_polygons, batch_img_sizes, batch_categories_ids):
         """
 
         :param batch_bboxes: list[bsize] of np.array[nti,4] entries, i=0:bsize . float Unormalized. [xc,yc,w,h]
@@ -27,26 +28,29 @@ class CreatesKptsEntries(CreatePolygons, CreateBboxes):
         # detection_entries = create_detection_entries(batch_bboxes, batch_img_sizes, batch_class_ids)
         entries=[]
 
-        for image_polygons, image_size, class_ids, image_bboxes in zip(batch_polygons, batch_img_sizes,
-                                                                  batch_class_ids, batch_bboxes):
+        nkpts = max(self.polygons_nvertices) # polygons with less verticess are 0 padded, for a uniform entry size
+        for image_polygons, image_size, image_categories_id, image_bboxes in zip(batch_polygons, batch_img_sizes,
+                                                                  batch_categories_ids, batch_bboxes):
 
             image_bboxes=np.array(image_bboxes)
-            image_polygons=np.array(image_polygons)
-
             im_height = image_size[0]
             im_width = image_size[1]
 
             # image_bboxes = (image_bboxes/np.array([im_width, im_height, im_width, im_height]))
-            img_kpts = (image_polygons/np.array([im_width, im_height]))
-            # concat occlusion  (=valid) field:
-            img_kpts_occlusion = np.full( [img_kpts.shape[0], img_kpts.shape[1], 1], 2.) # shape: [nobj, nkpts, 1]
-            img_kpts = np.concatenate([img_kpts, img_kpts_occlusion], axis=-1).reshape(img_kpts.shape[0], -1) # flatten kpts per object
+            img_kpts = []
+            for polygon in  image_polygons:
+                kpts = polygon/np.array([im_width, im_height])
+                pad = np.zeros([(nkpts - kpts.shape[0])*3])
+                occlusion_fields = np.full([kpts.shape[0], 1], 2.)
+                # concat occlusion  (=valid) field:
+                kpts = np.concatenate([kpts, occlusion_fields], axis=-1).reshape([-1])
+                kpts=np.append(kpts,pad)
+                img_kpts.append(kpts)
 
             img_entries=[]
-            category_id=0 # assumed a single category
-            for bbox, kpts   in zip(image_bboxes, img_kpts):
+            for bbox, kpts, category_id   in zip(image_bboxes, img_kpts, image_categories_id):
                 # box = ' '.join(str( round(vertex, 2)) for vertex in list(bbox))
-                box = ' '.join(str(vertex) for vertex in list(bbox))
+                box = ' '.join(str(round(vertex,3)) for vertex in list(bbox))
                 entry = f"{category_id} {box} {' '.join(str( round(kpt, 2)) for kpt in list(kpts.reshape(-1)))}"
                 img_entries.append(entry)
             entries.append(img_entries)
